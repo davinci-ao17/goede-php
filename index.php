798de1a9ce76c293139ff6291e482ac1a723699e
Brandon van Zanten <brandonvanzanten@gmail.com>
2018-01-24 11:01:33 +0100

    Goede-PHP V.1.0

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	
-</body>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title>Goede-PHP</title>
+	</head>
+	<body>
+		<?php
+		    date_default_timezone_set(timezone_name_from_abbr("", 3600, 0));
+            $time = date("H");
+            if ($time >= "6" && $time < "12") {
+                echo '<h1 id="greet">Goede morgen!</h1>'.'<p id="time">Het is nu ' . date("H:i") . '</p>'.'<style> body{ background-image: url(backgrounds/morning.png)}</style>';
+            } elseif ($time >= "12" && $time < "18") {
+                echo '<h1 id="greet">Goede middag!</h1>'.'<p id="time">Het is nu ' . date("H:i") . '</p>'.'<style> body{ background-image: url(backgrounds/afternoon.png)}</style>';
+            } elseif ($time >= "18" && $time <= "23") {
+                echo '<h1 id="greet">Goede avond!</h1>'.'<p id="time">Het is nu ' . date("H:i") . '</p>'.'<style> body{ background-image: url(backgrounds/evening.png)}</style>';
+            } elseif ($time < "6") {
+                echo '<h1 id="greet">Goede nacht!</h1>'.'<p id="time">Het is nu ' . date("H:i") . '</p>'.'<style> body{ background-image: url(backgrounds/night.png)}</style>';
+            }
+		?>
+	</body>
 </html>
